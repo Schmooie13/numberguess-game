c96b04c0f9824f53e42879eaeee61536e6c0b588
Shaun Lemoine <30362128+Schmooie13@users.noreply.github.com>
2017-08-08 20:57:51 -0500

    Add files via upload

--- GuessThatNumber.py
+++ GuessThatNumber.py
@@ -1,29 +1,36 @@
 #Guess That Number! v0.1
 #This is only my second program...let's see how it goes.
 
-#Print game prompt
-print ('# Welcome to My Little Lotto #')
-print ()
-print ('Your job in this game is guess the correct number(1-10).')
-print ('You will have four chances to get the number correct.')
-print ('Complete all four rounds and win!')
-print ()
-#Pull a random number into a variable
-import random
-random_num = random.randint(1,10)
-num_guesses = 4
-#Start guessing prompt
-while True:
-    guess = input('Please enter a number between 1-10: ')
+def main():
+    #Print game prompt
+    print ('# Welcome to Guess That Number v0.2 #')
+    print ('Your job in this game is guess the correct number(1-25).')
+    print ('You will have five chances to get the number correct.\n')
+    #Pull a random number into a variable
+    import random
+    random_num = random.randint(1,25)
+    num_guesses = 5
+    #Start guessing game
+    while True:
+        guess = input('Please enter a number between 1-25: ')
 
-    if int(guess) < 1 or int(guess) > 20:
-        print ('Invalid number. Please enter a number between 1-10.')
-    elif int(guess) == random_num:
-        print ('You have guessed correctly!~ You win!')
-        break
-    else:
-        num_guesses -= 1
-        print ('You have failed to guess correctly. You have ' + str(num_guesses) + ' try(s) left.')
-        if num_guesses == 0:
-            print ('You have no more guesses. Better luck next time.')
+        if int(guess) < 1 or int(guess) > 25:
+            print ('Invalid number. Please enter a number between 1-25.')
+        elif int(guess) == random_num:
+            print ('You have guessed correctly!~ You win!')
             break
+        else:
+            num_guesses -= 1
+            if num_guesses == 0:
+                print ('You have no more guesses. Better luck next time.')
+                print ("P.S. The correct number was %s." % random_num)
+                break
+            else:
+                if int(guess) < random_num:
+                    print ('You have failed to guess correctly. You have %s try(s) left.' % num_guesses)
+                    print ('Your number was less than target number.')
+                elif int(guess) > random_num:
+                    print ('You have failed to guess correctly. You have %s try(s) left.' % num_guesses)
+                    print ('Your number was higher than target number.')
+
+main()
